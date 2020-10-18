f3064a68d6c9e634150adba14634022c1eab660a
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-10-17 21:41:09 -0400

    finished header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Header />
+
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className='bananas'>Bananas for Scale</h1>
+    </header>
+  )
+}
